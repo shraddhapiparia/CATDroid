46606ea9267efda2a3a8126cd828a4fe7d66ed1e
Shraddha Piparia <shraddhapiparia@gmail.com>
2021-03-08 21:50:15 -0500

    Add files via upload

---
+++ main_tests/all_tests.py
@@ -0,0 +1,10 @@
+import unittest
+from main_tests.unit.completion_strategy_tests import CompletionStrategyTests
+from main_tests.unit.initialization_tests import InitializationTests
+from main_tests.unit.selection_strategy_tests import SelectionStrategyTests
+from main_tests.unit.selection_utils_tests import SelectionUtilsTests
+from main_tests.unit.termination_strategy_tests import TerminationStrategyTests
+from main_tests.integration.selection_tests import SelectionTests
+
+if __name__ == "__main__":
+    unittest.main()
