675120f4ef64b0bc86ad808e0c615725cbc0620f
Shraddha Piparia <shraddhapiparia@gmail.com>
2021-03-15 15:46:10 -0500

    Update adb.py

--- framework/utils/adb.py
+++ framework/utils/adb.py
@@ -39,43 +39,7 @@ def get_logs(adb_path, log_file_path, process_id, device_id):
     logger.info("Successfully retrieved log file: {}".format(log_file_path))
 
 # Added by Shraddha Piparia for Context actions
-def change_portrait(adb_path):
-    change_portrait_cmd = "{} {}".format(Script.CHANGE_PORTRAIT, adb_path)
-    subprocess.call(change_portrait_cmd, shell=True)
-    logger.info("Successfully changed to portrait")
-
-
-def change_landscape(adb_path):
-    change_landscape_cmd = "{} {}".format(Script.CHANGE_LANDSCAPE, adb_path)
-    subprocess.call(change_landscape_cmd, shell=True)
-    logger.info("Successfully changed to landscape")
-
-def power_on(adb_path):
-    power_on_cmd = "{} {}".format(Script.POWER_ON, adb_path)
-    subprocess.call(power_on_cmd, shell=True)
-    logger.info("Successfully turned on power")
-
-def power_off(adb_path):
-    power_off_cmd = "{} {}".format(Script.POWER_OFF, adb_path)
-    subprocess.call(power_off_cmd, shell=True)
-    logger.info("Successfully turned off power")
-
-def battery_high(adb_path):
-    battery_high_cmd = "{} {}".format(Script.SET_BATTERY_HIGH, adb_path)
-    subprocess.call(battery_high_cmd, shell=True)
-    logger.info("Successfully set battery high")
-
-def battery_low(adb_path):
-    battery_low_cmd = "{} {}".format(Script.SET_BATTERY_LOW, adb_path)
-    subprocess.call(battery_low_cmd, shell=True)
-    logger.info("Successfully set battery low")
-
-def internet_connected(adb_path):
-    internet_connected_cmd = "{} {}".format(Script.INTERNET_CONNECTED, adb_path)
-    subprocess.call(internet_connected_cmd, shell=True)
-    logger.info("Successfully connected internet")
-
-def internet_disconnected(adb_path):
-    internet_disconnected_cmd = "{} {}".format(Script.INTERNET_DISCONNECTED, adb_path)
-    subprocess.call(internet_disconnected_cmd, shell=True)
-    logger.info("Successfully disconnected internet")
+def change_context(adb_path, ch_landscape, ch_portrait, power_on, power_off, internet_on, internet_off, battery_1pc, battery_2pc, battery_5pc, battery_15pc, battery_ok, battery_high):
+    change_context_cmd = "{} {} {} {} {} {} {} {} {} {} {} {} {}".format(Script.CHANGE_CONTEXT, adb_path, ch_landscape, ch_portrait, power_on, power_off, internet_on, internet_off, battery_1pc, battery_2pc, battery_5pc, battery_15pc, battery_ok, battery_high)
+    subprocess.call(change_context_cmd, shell=True)
+    logger.info("Successfully changed context")
