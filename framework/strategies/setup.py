cfaf0f764925f6b7588f5dad9fdfbfa8b00ad12c
Shraddha Piparia <shraddhapiparia@gmail.com>
2021-04-19 18:59:04 -0500

    Update setup.py

--- framework/strategies/setup.py
+++ framework/strategies/setup.py
@@ -87,19 +87,3 @@ def _get_driver(apk_path, device_id):
 
     return driver
 
-
-def _get_driver(apk_path, device_id):
-    desired_caps = {
-        "platformName": "Android",
-        "deviceName": "Android Emulator",
-        "app": apk_path,
-        "newCommandTimeout": 3600,
-        "autoGrantPermissions": True,
-        "fullReset": True,
-        "disableWindowAnimation": True,
-        "appWaitActivity": "*",
-        "udid": device_id
-    }
-
-    driver = webdriver.Remote("http://localhost:4723/wd/hub", desired_caps)
-    return driver
