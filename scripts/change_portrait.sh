e3a3c4abb980ae90679aa73aefeb8d711cc9c03a
Shraddha Piparia <shraddhapiparia@gmail.com>
2021-04-19 19:11:29 -0500

    Delete change_portrait.sh

--- scripts/change_portrait.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# change to portrait
-
-ADB_PATH=$1
-$ADB_PATH shell content insert --uri content://settings/system --bind name:s:accelerometer_rotation --bind value:i:0
-$ADB_PATH  shell content insert --uri content://settings/system --bind name:s:user_rotation --bind value:i:1
