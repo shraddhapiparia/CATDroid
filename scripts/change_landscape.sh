14ee53b2c2481e8c7d43d22cff585145bcdf2e9e
Shraddha Piparia <shraddhapiparia@gmail.com>
2021-04-19 19:11:23 -0500

    Delete change_landscape.sh

--- scripts/change_landscape.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# change to landscape
-
-ADB_PATH=$1
-
-$ADB_PATH shell content insert --uri content://settings/system --bind name:s:accelerometer_rotation --bind value:i:0
-$ADB_PATH shell content insert --uri content://settings/system --bind name:s:user_rotation --bind value:i:0
