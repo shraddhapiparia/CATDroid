f101588a6525b9e8c87e13bc8cdc63769bb0d1dc
Shraddha Piparia <shraddhapiparia@gmail.com>
2021-04-19 19:04:24 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,27 +1,28 @@
 # Android settings
-ADB_PATH = "/home/UNT/sp0809/Android/Sdk/platform-tools/adb"
+ADB_PATH = "/home/davidadamojr/Android/Sdk/platform-tools/adb"
 
 # Autodroid settings
-OUTPUT_PATH = "/home/UNT/sp0809/git/autodroid/output"
-TOOL_PATH = "/home/UNT/sp0809/git/autodroid"
+OUTPUT_PATH = "/home/davidadamojr/git/autodroid/output"
+TOOL_PATH = "/home/davidadamojr/git/autodroid"
 EVENT_INTERVAL = 2
 TEST_SETUP = "Standard"
-EVENT_SELECTION_STRATEGY = "frequency_weighted"
-CONTEXT_STRATEGY = "pairs_interleaved"
+EVENT_SELECTION_STRATEGY = "random"
 TERMINATION_CRITERION = "Probabilistic"
 COMPLETION_CRITERION = "Time"
 TEST_TEARDOWN = "Standard"
 TEST_SUITE_LENGTH = 10  # only used if completion criterion is "Length"
-TIME_BUDGET = 7200  # (in seconds) only used if completion criterion is "Time"
+TIME_BUDGET = 900  # (in seconds) only used if completion criterion is "Time"
 TERMINATION_PROBABILITY = 0.05  # only used when termination criterion is "probabilistic"
 TEST_CASE_LENGTH = 20  # only used if termination criterion is "length"
 STRINGS_PATH = "strings.txt"
-COVERAGE_BROADCAST = "com.context.FINISH_TESTING"
+COVERAGE_BROADCAST = "com.davidadamojr.tester.finishtesting"
+TEST_SETUP = "iterative_start" # random_start or iterative_start
+COVERING_ARRAY_INDEX = 0
 
 # AUT settings
-APK_PATH = "/home/UNT/sp0809/git/autodroid/apps/app-prod-debug.apk"
-APP_PACKAGE_NAME = "com.greenaddress.abcore"
-COVERAGE_FILE_PATH = "/mnt/sdcard/com.greenaddress.abcore"
+APK_PATH = "/home/davidadamojr/git/autodroid/apps/org.tomdroid-0.7.5.apk"
+APP_PACKAGE_NAME = "org.tomdroid"
+COVERAGE_FILE_PATH = "/mnt/sdcard/org.tomdroid"
 
 # Device settings
 DEVICE_ID = "emulator-5554"
