20a7397b4c5f63ab6169860abab4f8ecc34766ca
Shraddha Piparia <shraddhapiparia@gmail.com>
2021-04-19 19:00:59 -0500

    Update factory.py

--- factory.py
+++ factory.py
@@ -7,6 +7,7 @@ from appiumatic.execution import Executor
 from appiumatic.exploration.sequence import SequenceGenerator
 from appiumatic.paths import create_output_directories
 from database import Database
+from appiumatic.exploration.context import ContextSequence
 
 import config
 import framework.initialization as initialization
@@ -17,14 +18,14 @@ logger = logging.getLogger(__name__)
 
 def create_database():
     db_connection = sqlite3.connect("db/autodroid.db")
-    # logger.debug("Connection to database successful.")
+    logger.debug("Connection to database successful.")
     database = Database(db_connection)
     database.create_tables()
     return database
 
 
-def create_explorer(database, text_values):
-    sequence_generator = create_sequence_generator(database, text_values)
+def create_explorer(database, text_values, context_covering_array):
+    sequence_generator = create_sequence_generator(database, text_values, context_covering_array)
     logger.debug("--------SEQUENCE GENERATOR CREATED----------")
     completion_criterion = initialization.completion_criterion(config.COMPLETION_CRITERION,
                                                                config.TIME_BUDGET,
@@ -38,27 +39,31 @@ def create_explorer(database, text_values):
     return Explorer(database, sequence_generator, completion_criterion, adb_info, app_info)
 
 
-def create_sequence_generator(database, text_values):
+def create_sequence_generator(database, text_values, context_covering_array):
     termination_criterion = initialization.termination_criterion(config.TERMINATION_CRITERION,
                                                                  config.TERMINATION_PROBABILITY,
                                                                  config.TEST_CASE_LENGTH)
     event_selection_strategy = initialization.event_selection_strategy(config.EVENT_SELECTION_STRATEGY)
+
+    context_sequence = ContextSequence(context_covering_array, config.COVERING_ARRAY_INDEX)
+
     setup_strategy = initialization.setup_strategy(config.TEST_SETUP,
                                                    config.APK_PATH,
                                                    config.ADB_PATH,
-                                                   config.DEVICE_ID)
+                                                   config.DEVICE_ID,
+                                                   context_sequence)
     tear_down_strategy = initialization.tear_down_strategy(config.TEST_TEARDOWN, config.ADB_PATH, config.DEVICE_ID)
     executor_factory = partial(create_executor,
                                event_interval=config.EVENT_INTERVAL,
                                text_values=text_values)
-    context_strategy = config.CONTEXT_STRATEGY
     sequence_generator = SequenceGenerator(database,
                                            termination_criterion,
                                            event_selection_strategy,
                                            setup_strategy,
                                            tear_down_strategy,
                                            executor_factory,
-                                           context_strategy)
+                                           context_sequence)
+
     return sequence_generator
 
 
